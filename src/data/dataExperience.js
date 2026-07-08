37054d65370ded97b51ceb51a7da01c53b01efb3
SolorzanoE <eduardo.solorzano.dev@gmail.com>
2026-07-07 22:53:50 -0600

    Feat: Add first data for experience

--- src/data/dataExperience.js
+++ src/data/dataExperience.js
@@ -1,8 +1,34 @@
 export const dataExperiencie = [
   {
-    title: "Something"
+    title: "Practicas profesionales",
+    company: "INIFAP",
+    startDate: new Date("2024-6-1"), 
+    endDate: new Date("2024-12-1"), 
+    description: "", 
+    technologies: ["Java", "PostgreSQL", "Desktop"]
   },
   {
-    title: "Another"
+    title: "Desarrollador fullstack",
+    company: "Freelance - Subcontratado",
+    startDate: new Date("2024-12-15"), 
+    endDate: new Date("2025-09-26"), 
+    description: "", 
+    technologies: ["Spring Framework", "PostgreSQL", "React", "JavaScript", "Java", "MUI", "Web"]
+  },
+  {
+    title: "Servicio social",
+    company: "Compusur",
+    startDate: new Date("2025-12-22"), 
+    endDate: new Date("2026-6-27"), 
+    description: "", 
+    technologies: ["React", "JavaScript", "MUI", "Web"]
+  },
+  {
+    title: "Residencia profesional",
+    company: "Compusur",
+    startDate: new Date(),
+    endDate: new Date(), 
+    description: "", 
+    technologies: ["Spring Framework", "PostgreSQL", "React", "TypeScript", "MUI", "Web"]
   }
 ]
