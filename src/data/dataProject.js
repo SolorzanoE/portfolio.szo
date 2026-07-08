eeeba5dfce1f0d643f69941e82a750d8c2aed421
SolorzanoE <eduardo.solorzano.dev@gmail.com>
2026-07-07 23:01:41 -0600

    Feat: Add first data project

--- src/data/dataProject.js
+++ src/data/dataProject.js
@@ -1,11 +1,24 @@
 export const dataProject = [
   {
-    title: "Something"
+    title: "Inventory-Qi",
+    description: "Sistema de consulta y monitoreo de existencias, diseñado para la visualización de datos provenientes de CONTPAQi® Comercial.",
+    image: "",
+    technologies: ["React", "JavaScript", "MUI", "Vite", "C#", "ASP.NET"],
+    repoUrl: "https://github.com/SolorzanoE/inventory-qi"
   },
   {
-    title: "Another"
+    title: "Serviicy",
+    description: "Sistema gestor de pólizas de usuarios, orientado al soporte de servicios.",
+    image: "",
+    technologies: ["Spring Boot", "PostgreSQL", "TypeScript", "React", "Vite"],
+    //demoUrl: "1",
+    repoUrl: ""
   },
   {
-    title: "IDK"
+    title: "Data tracking Cacao",
+    description: "",
+    image: "",
+    technologies: ["Java", "PostgreSQL"],
+    repoUrl: ""
   }
 ]
