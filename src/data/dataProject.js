b3d625dbc75112a939f6cc84ebbb1276a9683e41
SolorzanoE <eduardo.solorzano.dev@gmail.com>
2026-08-05 21:25:52 -0600

    style: change position project

--- src/data/dataProject.js
+++ src/data/dataProject.js
@@ -4,18 +4,18 @@ import cacaoImage from "@/assets/cacao-image.png"
 
 export const dataProject = [
   {
-    title: "Inventory-Qi",
-    description: "Sistema de consulta y monitoreo de existencias, diseñado para la visualización de datos provenientes de CONTPAQi® Comercial.",
-    image: inventoryImage,
-    technologies: ["React", "JavaScript", "MUI", "Vite", "C#", "ASP.NET"],
-    repoUrl: "https://github.com/SolorzanoE/inventory-qi"
-  },
-  {
     title: "Serviicy",
     description: "Sistema gestor de pólizas de usuarios, orientado al soporte de servicios.",
     image: serviicyImage,
     technologies: ["Spring Boot", "PostgreSQL", "TypeScript", "React", "Vite"],
     //demoUrl: "1",
+  },
+  {
+    title: "Inventory-Qi",
+    description: "Sistema de consulta y monitoreo de existencias, diseñado para la visualización de datos provenientes de CONTPAQi® Comercial.",
+    image: inventoryImage,
+    technologies: ["React", "JavaScript", "MUI", "Vite", "C#", "ASP.NET"],
+    repoUrl: "https://github.com/SolorzanoE/inventory-qi"
   },
   {
     title: "Data tracking Cacao",
